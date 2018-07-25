b1c8dbf1317e76c130203ab8bdfc7ef12878ca88
Raúl Doezón <santor3medio@gmail.com>
2018-07-25 09:33:36 -0500

    #1. Add the carousel functionality to the home apartments section. Add in home-apartments-carousel.js

---
+++ src/js/home-apartments-carousel.js
@@ -0,0 +1,27 @@
+var homeApartmentsCarousel = $('.home-apartments-carousel');
+
+homeApartmentsCarousel.owlCarousel ({
+  loop: false,
+  nav: true,
+  dots: true,
+  navText: ['<img class="home-apartments-carousel-navigation" src="/img/icons/icon-nav-left.png" />', '<img class="home-apartments-carousel-navigation" src="/img/icons/icon-nav-right.png" />'],
+  margin: 50,
+  responsive:{
+    0:{
+      items:1
+    },
+    700:{
+      items: 1
+    },
+    880: {
+      items: 1
+    },
+    1000:{
+      items:1
+    }
+  }
+});
+
+homeApartmentsCarousel.each(function(index) {
+  $(this).find('.owl-nav, .owl-dots').wrapAll("<div class='home-apartments-carousel-controls'></div>");
+});
