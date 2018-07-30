3253f2ba2fd1f30af6eca149a42a1066505ce500
Raúl Doezón <santor3medio@gmail.com>
2018-07-30 17:48:17 -0500

    #5. Changes in home-apartments-carousel.js

--- src/js/home-apartments-carousel.js
+++ src/js/home-apartments-carousel.js
@@ -1,11 +1,13 @@
 var homeApartmentsCarousel = $('.home-apartments-carousel');
 
 homeApartmentsCarousel.owlCarousel ({
-  loop: false,
+  loop: true,
   nav: true,
   dots: true,
   navText: ['<img class="home-apartments-carousel-navigation" src="/img/icons/icon-nav-left.png" />', '<img class="home-apartments-carousel-navigation" src="/img/icons/icon-nav-right.png" />'],
-  margin: 50,
+  margin: 0,
+  autoWidth: true,
+  center: true,
   responsive:{
     0:{
       items:1
