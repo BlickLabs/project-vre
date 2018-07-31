8fa2e1187c22b5698cd8c105673d38f49474c6d4
Raúl Doezón <santor3medio@gmail.com>
2018-07-31 16:35:12 -0500

    #5. Add more classes to apply the functionality. Changes in app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,17 @@
 $(function(){
-  $(".nav-icon-phone, .nav-icon-whatsapp").click(function(){
-    $(".nav-icon-info-mobile").fadeToggle(0);
+  $(".nav-icon-phone").click(function(){
+    $(".nav-icon-info-mobile-phone").fadeToggle(0);
+  });
+
+  $(".nav-icon-whatsapp").click(function(){
+    $(".nav-icon-info-mobile-whats").fadeToggle(0);
+  });
+
+  $(".nav-icon-phone-mobile").click(function(){
+    $(".nav-icon-info-mobile-phone").fadeToggle(0);
+  });
+
+  $(".nav-icon-whatsapp-mobile").click(function(){
+    $(".nav-icon-info-mobile-whats").fadeToggle(0);
   });
 });
