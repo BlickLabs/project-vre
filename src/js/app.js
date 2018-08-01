a5e765b9030d8eed79d26c3bde70f71e0083e732
Raúl Doezón <santor3medio@gmail.com>
2018-08-01 16:28:37 -0500

    #5. Include the functionality to close the tooltip of the whatsapp and phone icons in navbar mobile and hamburger menu open. Changes in app.js

--- src/js/app.js
+++ src/js/app.js
@@ -8,10 +8,30 @@ $(function(){
   });
 
   $(".nav-icon-phone-mobile").click(function(){
-    $(".nav-icon-info-mobile-phone").fadeToggle(0);
+    $(".nav-icon-info-mobile-hamburger-phone").fadeToggle(0);
   });
 
   $(".nav-icon-whatsapp-mobile").click(function(){
-    $(".nav-icon-info-mobile-whats").fadeToggle(0);
+    $(".nav-icon-info-mobile-hamburger-whats").fadeToggle(0);
+  });
+
+
+  $(document).on("click", function(e){
+    var $target = $(e.target);
+    if(!$target.is(".nav-icon-phone") && !$target.is(".nav-icon-info-mobile-phone")){
+        $(".nav-icon-info-mobile-phone").fadeOut(0);
+    }
+
+    if(!$target.is(".nav-icon-whatsapp") && !$target.is(".nav-icon-info-mobile-whats")){
+        $(".nav-icon-info-mobile-whats").fadeOut(0);
+    }
+
+    if(!$target.is(".nav-icon-phone-mobile") && !$target.is(".nav-icon-info-mobile-hamburger-phone")){
+        $(".nav-icon-info-mobile-hamburger-phone").fadeOut(0);
+    }
+
+    if(!$target.is(".nav-icon-whatsapp-mobile") && !$target.is(".nav-icon-info-mobile-hamburger-whats")){
+        $(".nav-icon-info-mobile-hamburger-whats").fadeOut(0);
+    }
   });
 });
